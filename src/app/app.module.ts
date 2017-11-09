0d2b005a9b827ac885d15599ee5e72b0f5bb641e
Daniel Tade <inplaceuk@me.com>
2017-11-09 01:04:31 +0000

    more files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { Routes, RouterModule } from '@angular/router';
+//components
 import { AppComponent } from './app.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { VideoItemComponent } from './components/video-item/video-item.component';
+//services
+import {MusicplayService} from '../services/music-play.service';
+//Pipes
+import {TruncatePipe} from '../pipes/truncate-pipe';
+import {DateFilterPipe} from '../pipes/date-filter-pipe';
+//App routes
+export const appRoutes: Routes = [
+  { path: '', component: PlaylistComponent },
+  { path: 'playlist/:videoId', component: VideoItemComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     PlaylistComponent,
+    VideoItemComponent,
+    TruncatePipe,
+    DateFilterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+     MusicplayService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
